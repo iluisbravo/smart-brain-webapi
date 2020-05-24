3d7ca3d5f5b0c8f01c49e1644f1e6fa290d89722
iluisbravotis <ing.luisalbertobravo@gmail.com>
2020-05-24 13:03:24 -0700

    fixing port

--- server.js
+++ server.js
@@ -198,6 +198,8 @@ app.post('/imageURL', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("Server is running in port: 3000.")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running in port: ${ PORT }.`);
 });
