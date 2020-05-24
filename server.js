1702ff1ca36a393a3551224308724036faacc3ef
iluisbravotis <ing.luisalbertobravo@gmail.com>
2020-05-24 13:54:18 -0700

    process.env.NODE_TLS_REJECT_UNAUTHORIZED

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const knex = require('knex');
 const Clarifai =  require('clarifai');
 
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
 const db = knex({
     client: 'pg',
         connection: {
